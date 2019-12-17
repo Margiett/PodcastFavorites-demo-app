7c01296493eccfc3c0b25128830849ddb837d979
Margiett Gil <mxg627@icloud.com>
2019-12-17 15:29:27 -0500

    Initial Commit

---
+++ PodcastFavorites demo app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodcastFavorites demo app
+//
+//  Created by Margiett Gil on 12/17/19.
+//  Copyright © 2019 Margiett Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
